1186d3e530902d184643805f2b7da2fbbcdc8224
ge34jit <ge34jit@mytum.de>
2022-10-05 00:21:13 +0200

    leetcode #35 - Search Insert Position (Binary Search)

---
+++ easy/0035_SearchInsertPosition.js
@@ -0,0 +1,59 @@
+/*
+35. Search Insert Position
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+
+Link:
+https://leetcode.com/problems/search-insert-position/
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var searchInsert = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    let currentIndex;
+    
+    while(start <= end) {
+        if(target > nums[end])
+            return end + 1;
+        if(target < nums[0])
+            return 0;
+
+        currentIndex = Math.ceil((start + end) / 2);
+        if (target == nums[currentIndex])
+            return currentIndex;
+        if (target > nums[currentIndex])
+            start = currentIndex + 1;
+        if (target < nums[currentIndex]) 
+            end = currentIndex - 1;
+    }
+    
+    return currentIndex;
+};
+
+let arr = [1, 3, 6, 9, 12, 31];
+let index = searchInsert(arr, 6);
+console.log(index);
