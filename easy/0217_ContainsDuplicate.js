650d149ff4be5b04dbb8edc37e651ff9ba469f4d
ge34jit <ge34jit@mytum.de>
2023-01-17 11:34:14 +0100

    edited function name

--- easy/0217_ContainsDuplicate.js
+++ easy/0217_ContainsDuplicate.js
@@ -26,7 +26,7 @@ Link: https://leetcode.com/problems/contains-duplicate/description/
  * @return {boolean}
  */
 // my initial solution
-var containsDuplicate = function(nums) {
+var containsDuplicateInitial = function(nums) {
     let hashTable = {}
     for(let i=0; i<nums.length; i++) {
         hashTable[nums[i]] = []
