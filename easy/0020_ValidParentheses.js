149ec90384e44e78e6e2ae7af6a2c7861a41ec87
ge34jit <ge34jit@mytum.de>
2023-01-22 11:15:31 +0100

    20.ValidParentheses solution

--- easy/0020_ValidParentheses.js
+++ easy/0020_ValidParentheses.js
@@ -28,3 +28,42 @@ s consists of parentheses only '()[]{}'.
 Link: https://leetcode.com/problems/valid-parentheses/
 */
 
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = []
+    if(s.length % 2 === 1)
+        return false
+
+    stack.push(s[0])
+    let i = 1
+    while(i < s.length) {
+        if(!isOpener(s[i])) {
+            if(isLegitPair(stack[stack.length - 1], s[i])) {
+                stack.pop()
+            }
+            else 
+                return false
+        }
+        else 
+            stack.push(s[i])
+        i++
+    }
+    return stack.length === 0
+};
+
+var isLegitPair = function(c1, c2) {
+    return c1 === '(' && c2 === ')' || 
+            c1 === '{' && c2 === '}' ||
+            c1 === '[' && c2 === ']'
+}
+
+var isOpener = function(c) {
+    return c === '{' || c === '[' || c === '('
+}
+
+let sample = "([}}])"
+console.log(isValid(sample))
+
