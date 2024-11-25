10e166fb060a5726a5f33638182909972e45bf47
Bakar Andguladze <bakar.andguladze@in-tech.com>
2024-11-25 21:31:07 +0100

    test commit 3

--- easy/1480_RunningSum.js
+++ easy/1480_RunningSum.js
@@ -15,12 +15,12 @@ Output: [1,2,3,4,5]
 Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
 
 Example 3:
-Input: nums = [3,1,2,10,1] 
+Input: nums = [3,1,2,10,1]
 Output: [3,4,6,16,17]
 
 Constraints:
 1 <= nums.length <= 1000
--10^6 <= nums[i] <= 10^6 
+-10^6 <= nums[i] <= 10^6
 
 Link: https://leetcode.com/problems/running-sum-of-1d-array/description/
 */
