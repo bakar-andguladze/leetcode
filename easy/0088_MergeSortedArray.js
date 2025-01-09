dd7b045a6524fe556e80de8490c51d7121a81850
bakar-andguladze <baqar.andghuladze@gmail.com>
2025-01-09 11:26:44 +0100

    add sample array

--- easy/0088_MergeSortedArray.js
+++ easy/0088_MergeSortedArray.js
@@ -49,3 +49,11 @@ var merge = function(nums1, m, nums2, n) {
         p2--
     }
 };
+
+let nums1 = [1,2,3,0,0,0]
+let nums2 = [2,5,6]
+let m = 3
+let n = 3
+
+merge(nums1, m, nums2, n)
+console.log(nums1) // [1,2,2,3,5,6]
